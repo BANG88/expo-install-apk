b0e45def069d27b2702fb22388daee2cbb3e6449
BANG88 <sqibang@gmail.com>
2024-03-11 19:11:25 +0800

    Update package version and add platform check in install function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import { Platform } from "react-native";
+
 import ExpoInstallApkModule from "./ExpoInstallApkModule";
 
 export function install(filePath: string, fileProviderAuthority: string): void {
   try {
+    if (Platform.OS !== "android") {
+      return;
+    }
     if (!filePath) throw new Error("filePath is required");
     if (!fileProviderAuthority)
       throw new Error("fileProviderAuthority is required");
